ba2142ab1a78ea3704d1df62838a7b6cd36f7f9b
Cian McElhinney <cianmce@gmail.com>
2015-12-21 19:12:20 +0000

    commit

--- server.rb
+++ server.rb
@@ -33,7 +33,7 @@ class Server
           while @running
             if work_q.length > 0
               client = work_q.pop
-              handle_request(client, Thread.current['id'])
+              handle_request(client)
             else
               sleep(0.05)
             end
@@ -66,12 +66,12 @@ class Server
     @logger.info msg
   end
 
-  def handle_request(client, tid)
+  def handle_request(client)
     # data = client.gets # Read 1st line from socket
 
     while true
       # sleep(0.05)
-      info "Reading[#{tid}]..."
+      info "Reading[#{Thread.current['id']}]..."
       line = client.gets
       # data = client.readpartial(MAX_READ_CHUNK) # Read data
       info "Read"
